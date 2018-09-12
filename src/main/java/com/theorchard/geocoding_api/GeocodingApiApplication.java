1158d5adb9b7f002abd80bf0bc68fadf63b73bb1
bpetersen <b.petersen@finetunes.net>
2018-09-12 11:32:21 +0200

    Initial commit

---
+++ src/main/java/com/theorchard/geocoding_api/GeocodingApiApplication.java
@@ -0,0 +1,12 @@
+package com.theorchard.geocoding_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeocodingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeocodingApiApplication.class, args);
+	}
+}
